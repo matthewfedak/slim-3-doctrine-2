3636ea318fc289a02d08c69dbbc50878f19be609
Matthew Fedak <matthewfedak@hotmail.com>
2015-10-31 19:41:28 +0000

    Added a new entity called photo

---
+++ app/src/Entity/Photo.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Entity;
+
+use App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="photos")
+ */
+class Photo
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    protected $image;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $slug;
+
+    /**
+     * Get array copy of object
+     *
+     * @return array
+     */
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
+
+    /**
+     * Get photo id
+     *
+     * @ORM\return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get photo title
+     *
+     * @ORM\return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Get photo slug
+     *
+     * @ORM\return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Get photo image
+     *
+     * @ORM\return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
