ffdcc6de47c71b35eb2fd468a8cd4c414ddeda2c
Matthew Fedak <matthewfedak@hotmail.com>
2015-10-31 19:42:22 +0000

    Setup doctrine as a dependency in the app

--- app/dependencies.php
+++ app/dependencies.php
@@ -37,6 +37,19 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
+// Doctrine
+$container['em'] = function ($c) {
+    $settings = $c->get('settings');
+    $config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(
+        $settings['doctrine']['meta']['entity_path'],
+        $settings['doctrine']['meta']['auto_generate_proxies'],
+        $settings['doctrine']['meta']['proxy_dir'],
+        $settings['doctrine']['meta']['cache'],
+        false
+    );
+    return \Doctrine\ORM\EntityManager::create($settings['doctrine']['connection'], $config);
+};
+
 // -----------------------------------------------------------------------------
 // Action factories
 // -----------------------------------------------------------------------------
